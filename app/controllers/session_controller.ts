94bcfbb6ee1920271b1f94aaa49f3f95d782962e
Hermes Mavinga <hermesmavinga2019@gmail.com>
2025-10-17 22:39:13 +0100

    validation login

--- app/controllers/session_controller.ts
+++ app/controllers/session_controller.ts
@@ -1,14 +1,20 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import User from '#models/user'
+import { loginValidator } from '#validators/sign_up'
 
 export default class SessionController {
   public async store({ auth, request, response }: HttpContext) {
-    const { email, password } = request.only(['email', 'password'])
+    // const { email, password } = request.only(['email', 'password'])
 
+    // const user = await User.verifyCredentials(email, password)
+
+    // await auth.use('web').login(user)
+
+    // response.redirect('/dashboardView')
+
+    const { email, password } = await request.validateUsing(loginValidator)
     const user = await User.verifyCredentials(email, password)
-
     await auth.use('web').login(user)
-
     response.redirect('/dashboardView')
   }
 }
